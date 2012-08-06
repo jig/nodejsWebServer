f4abdc69cc882341beaf4df0cda81659aebe9383
jig <jig@scryptor>
2012-08-06 09:39:21 +0200

    Added samples reading
    Now it supports append json files, listing and reading

--- launch.js
+++ launch.js
@@ -6,10 +6,13 @@ var fs = require('fs');
 var server = require("./server");
 
 args
-	.version('0.0.1')
+	.version('0.1')
 	.option('-p, --port [number]', 'client port', 80)
 	.option('-f, --folder [folderpath]', 'root folder path', './' )
 	.parse(process.argv);
+
+console.log('Binding to port ', args.port);
+console.log('Folder path: ', args.folder);
 
 // client pages, styles and scripts
 var extensions = {
@@ -24,59 +27,118 @@ var extensions = {
 	""		: "text/plain"
 }
 
-var handle = {};
+var handle = {
+	"": function (pathname, response) {
+		// known bug: does not understant filenames with dots in it:
+		var extension = '';
+		var regex = pathname.match( /^\/([^\.]*\.(.*))$/ );
+		if(regex) {
+			pathname = regex[1];
+			extension = regex[2].toLowerCase();
+		}
+		fs.readFile(args.folder + pathname, function (err, filedata) {
+			if (err) {
+				fs.readdir(args.folder + pathname, function (err, fileNames) {
+					if(err) {
+						console.log("No request handler found nor file for " + pathname);
+						response.writeHead(404, { "Content-Type": extensions.text });
+						response.write("404 Not found\n" + err);
+						response.end();
+					} else {
+						console.log("Read dir " + args.folder + pathname);
+						response.writeHead(200, { "Content-Type": extensions.html });
+						for(key in fileNames) {
+							response.write('<div class="direntry"><a href="' + fileNames[key] + '">' + fileNames[key] + '</a></div>');
+						}
+						response.end();
+					}
+				});
+			} else {
+				console.log("Read file (" + extension + ': ' + extensions[extension] + "): " + pathname);
+				response.writeHead(200, { "Content-Type": extensions[extension] });
+				response.write(filedata);
+				response.end();
+			}
+		});
+	},
 
-// file handlers (index.html shorcut not supported)
-handle[null] = function (pathname, response) {
-	// known bug: does not understant filenames with dots in it:
-	var regex = pathname.match( /^\/([^\.]*\.(.*))$/ );
-	var extension;
-	if(regex) {
-		extension = regex[2].toLowerCase();
-		pathname = regex[1];
-	} else {
-		extension = '';
-		pathname = '';
+	// sample: gets a JSON POST and stores it in a file (directory _APPEND folder must exist):
+	"/append": function (response, postData) {
+		response.writeHead(200, {"Content-Type": extensions.json });
+		var fileName = args.folder + '_APPEND/A_' + ((new Date()).getTime()) + '.json';
+			console.log('Write to ' + fileName + ' data:' + postData);
+		fs.writeFile(fileName, postData, function (err) {
+			if (err) {
+				response.write('resp({ "status": true, "writeLogStatus": false });');
+			} else {
+				response.write('resp({ "status": true, "writeLogStatus": true });');
+			}
+			response.end();
+		});
+	},
+
+	"/datalist": function (response, postData) {
+		var folder = args.folder + '/_APPEND';
+		fs.readdir(folder, function (err, fileNames) {
+			if(!err) {
+				var datas = new Array();
+				for(var i=0; i<fileNames.length; ++i) {
+					fs.readFile(folder + '/' + fileNames[i], 'utf8', function (err, filedata) {
+						if(err) {
+							console.log(err);
+						} else {
+							// en asincron no puc passar el nom del fitxer??: console.log(i + ':' + folder + '/' + fileNames[i]);
+							var object = JSON.parse(filedata);
+							datas.push( object );
+							if(datas.length===fileNames.length) {
+								console.log('Files read: ' + datas.length);
+								response.writeHead(200, {"Content-Type": extensions.text });
+								response.write(JSON.stringify(datas));
+								response.end();
+							}
+						}
+					});
+				}
+			}
+		});
+	},
+	
+	"/": function (response, postData, urlElements) {
+		var query = urlElements.query;
+		if(query.samples !== undefined) {
+			console.log('Reading samples list');
+			fs.readdir(args.folder + '/_APPEND/', function (err, fileNames) {
+				if(!err) {	
+					response.writeHead(200, {"Content-Type": extensions.json });
+					response.write(JSON.stringify(fileNames));
+					response.end();
+				}
+			});
+		} else if(query.sample !== undefined) {
+			console.log('Reading samples file: ' + query.sample);
+			fs.readFile(args.folder + '/_APPEND/' + query.sample, 'utf8', function (err, fileData) {
+				if(!err) {	
+					response.writeHead(200, {"Content-Type": extensions.json });
+					response.write(fileData);
+					response.end();
+				}
+			});
+		} else if(query.sample !== undefined) {
+			console.log(': ' + query.sample);
+			fs.readFile(args.folder + '/_APPEND/' + query.sample, 'utf8', function (err, fileData) {
+				if(!err) {	
+					response.writeHead(200, {"Content-Type": extensions.json });
+					response.write(fileData);
+					response.end();
+				}
+			});
+		} else {
+			console.log('Wrong command (' + JSON.stringify(urlElements.query) + ').');
+			response.writeHead(200, {"Content-Type": extensions.text });
+			response.write('Wrong command (' + JSON.stringify(urlElements.query) + ').');
+			response.end();
+		}
 	}
-	console.log("About to read file (" + extension + ':' + extensions[extension] + ")" + pathname);
-	fs.readFile(args.folder + pathname, function (err, filedata) {
-		if (err) {
-			console.log("No request handler found nor file for " + pathname);
-			response.writeHead(404, {
-					"Content-Type": "text/plain"
-				});
-			response.write("404 Not found");
-			response.end();
-		} else {
-			response.writeHead(200, {
-					"Content-Type": extensions[extension]
-				});
-			response.write(filedata);
-			response.end();
-		}
-	});
-
-}
-
-// sample: gets a JSON POST and stores it in a file:
-handle["/append"] = function (response, postData) {
-	response.writeHead(200, {"Content-Type": extensions.json });
-	var fileName = args.folder + '_APPEND/A_' + ((new Date()).getTime()) + '.json';
-		console.log('write to ' + fileName + ' data:' + postData + '\n');
-	fs.writeFile(fileName, 'var mostra = \n\t' + postData + '\n;\n', function (err) {
-		if (err) {
-			response.write('resp({ "status": false });');
-		} else {
-			response.write('resp({ "status": true });');
-		}
-		response.end();
-	});
-}
-
-handle["/"] = function (response) {
-  response.writeHead(200, {"Content-Type": extensions.text });
-  response.write("Virtual Keyboard Test Facility");
-  response.end();
-}
+};
 
 server.start(args.port, handle);
--- server.js
+++ server.js
@@ -4,36 +4,30 @@
 var http = require("http");
 var url = require("url");
 
-function route(handle, pathname, response, postData) {
-  console.log("About to route a request for " + pathname);
-  if (typeof handle[pathname] === 'function') {
-	handle[pathname](response, postData);
-  } else {
-	handle[null](pathname, response, postData);
-  }
+function route(handle, urlElements, response, postData) {
+	if (typeof handle[urlElements.pathname] === 'function') {
+		handle[urlElements.pathname](response, postData, urlElements);
+	} else {
+		handle[''](urlElements.pathname, response, postData, urlElements);
+	}
 }
 
 function start(port, handle) {
 	function onRequest(request, response) {
 		var postData = '';
-		var pathname = url.parse(request.url).pathname;
-		// console.log("\nRequest: " + pathname);
 
 		request.setEncoding('utf8');
 	    request.addListener("data", function(postDataChunk) {
 			postData += postDataChunk;
-			// console.log("Received POST data chunk '" + postDataChunk + "'.");
 	    });
 
 		request.addListener("end", function() {
-			// enruta al handler apropiat
-			// console.log("Received POST **TOTAL** data '" + postData + "'.");
-			route(handle, pathname, response, postData);
+			route(handle, url.parse(request.url, true), response, postData);
 		});
 	}
 
 	http.createServer(onRequest).listen(port);
-	console.log("Server has started at port " + port);
+	console.log("Server started.");
 }
 
 exports.start = start;
